5635704aa743d8b795cba45f1b906641c56b82e9
Chanhwi Lee <chanhwi.lee6@gmail.com>
2022-05-08 23:03:51 +0900

    send arduino

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const PORT = 3000;
 
 const app = express();
 
+let topAngleServer;
+let bottomAngleServer;
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  console.log("data sent to Arduino");
   res.send(
-    `
-        <h1>HI THERE!</h1>
-        `
+    JSON.stringify({ topAngle: topAngleServer, bottomAngle: bottomAngleServer })
   );
 });
 
 app.post("/", (req, res) => {
   console.log(req.body);
+  topAngleServer = req.body.topAngle;
+  bottomAngleServer = req.body.bottomAngle;
 });
 
 app.listen(PORT, () => console.log("Listening port 3000"));
